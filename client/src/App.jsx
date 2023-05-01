85f2ab892cc7d7884a91e4a779765d1ca4f872b8
glenpereira <glenv.pereira@gmail.com>
2023-05-01 16:17:51 +0530

    initial working build

---
+++ client/src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import axios from "axios";
+const API_SOURCE = import.meta.env.VITE_MARIABOT_API_URL;
+const MODEL_API_URL = `http://${API_SOURCE}/text`;
+
+const App = () => {
+  const [inputText, setInputText] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const text = {
+      text: inputText,
+    };
+    await axios
+      .post(MODEL_API_URL, text, { responseType: "blob" })
+      .then((res) => {
+        //create file link in browser's memory
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+
+        //create "a" HTML element with href to file and click
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "sample.wav"); // or any other extension
+        document.body.appendChild(link);
+        link.click();
+
+        //clean up "a" element & remove objecturl
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div>
+      <div className="title">
+        <h1>MariaBot</h1>
+        <div className="title-caption">
+          <h2>Malayalam TTS using Tacotron2 and Waveglow</h2>
+        </div>
+      </div>
+
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="text-form">
+          <label>
+            Type here:{" "}
+            <textarea
+              name="myInput"
+              type="text"
+              value={inputText}
+              placeholder="Type Manglish text here."
+              onChange={(e) => setInputText(e.target.value)}
+              className="input-field"
+            />
+          </label>
+          <button type="submit" className="submit-button">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default App;
